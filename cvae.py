267545c188d16e16a1ce7d72b9728840d4f00b61
Kovalenko Alexey <alexey.s.russ@gmail.com>
2018-10-08 07:42:04 +0300

    Add text image generator

--- cvae.py
+++ cvae.py
@@ -109,3 +109,80 @@ def create_cvae(input_shape, num_classes, latent_dim, dropout_rate, batch_size,
     models["cvae"].compile(optimizer=Adam(start_lr), loss=vae_loss)
 
     return models, vae_loss
+
+
+def create_simple_cvae(input_shape, num_classes, latent_dim, dropout_rate, batch_size,
+                start_lr=0.001):
+    models = {}
+
+    # Добавим Dropout и BatchNormalization
+    def apply_bn_and_dropout(x):
+        return Dropout(dropout_rate)(BatchNormalization()(x))
+
+    # Энкодер
+    input_img = Input(shape=(input_shape[0], input_shape[1], 1))
+    flatten_img = Flatten()(input_img)
+    input_lbl = Input(shape=(num_classes,), dtype='float32')
+
+    x = concatenate([flatten_img, input_lbl])
+    x = Dense(256, activation='relu')(x)
+    x = apply_bn_and_dropout(x)
+
+    # Предсказываем параметры распределений
+    # Вместо того чтобы предсказывать стандартное отклонение,
+    # предсказываем логарифм вариации
+    z_mean = Dense(latent_dim)(x)
+    z_log_var = Dense(latent_dim)(x)
+
+    # Сэмплирование из Q с трюком репараметризации
+    def sampling(args):
+        z_mean, z_log_var = args
+        epsilon = K.random_normal(shape=(batch_size, latent_dim), mean=0.,
+                                  stddev=1.0)
+        return z_mean + K.exp(z_log_var / 2) * epsilon
+
+    l = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_var])
+
+    models["encoder"] = Model([input_img, input_lbl], l, 'Encoder')
+    models["z_meaner"] = Model([input_img, input_lbl], z_mean, 'Enc_z_mean')
+    models["z_lvarer"] = Model([input_img, input_lbl], z_log_var,
+                               'Enc_z_log_var')
+
+    # Декодер
+    z = Input(shape=(latent_dim,))
+    input_lbl_d = Input(shape=(num_classes,), dtype='float32')
+
+    x = concatenate([z, input_lbl_d])
+    x = Dense(256)(x)
+    x = LeakyReLU()(x)
+    x = apply_bn_and_dropout(x)
+    x = Dense(input_shape[0] * input_shape[1], activation='sigmoid')(x)
+    decoded = Reshape((input_shape[0], input_shape[1], 1))(x)
+
+    models["decoder"] = Model([z, input_lbl_d], decoded, name='Decoder')
+    models["cvae"] = Model([input_img, input_lbl, input_lbl_d],
+                           models["decoder"](
+                               [models["encoder"]([input_img, input_lbl]),
+                                input_lbl_d]),
+                           name="CVAE")
+    models["style_t"] = Model([input_img, input_lbl, input_lbl_d],
+                              models["decoder"](
+                                  [models["z_meaner"]([input_img, input_lbl]),
+                                   input_lbl_d]),
+                              name="style_transfer")
+
+    def vae_loss(x, decoded):
+        x = K.reshape(x, shape=(batch_size, input_shape[0] * input_shape[1]))
+        decoded = K.reshape(decoded,
+                            shape=(batch_size, input_shape[0] * input_shape[1]))
+        xent_loss = input_shape[0] * input_shape[1] * \
+                    binary_crossentropy(x, decoded)
+        kl_loss = -0.5 * K.sum(
+            1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1
+        )
+        return (xent_loss + kl_loss) / 2 / input_shape[0] / input_shape[1]
+
+    models["cvae"].compile(optimizer=Adam(start_lr), loss=vae_loss)
+
+    return models, vae_loss
+
--- generate_handwritten.py
+++ generate_handwritten.py
@@ -1,31 +1,9 @@
-from cvae import create_cvae
+from cvae import create_cvae, create_simple_cvae
 from train_cvae import classes, read_sample, one_hot_vector
 import argparse
 from keras.models import load_model
 import numpy as np
 import cv2
-
-
-def argument_parser():
-    arg_pars = argparse.ArgumentParser()
-    arg_pars.add_argument('text', nargs='+',
-                          help='Text for translate.')
-    arg_pars.add_argument('--base-image', required=True,
-                          help='Base style image path.')
-    arg_pars.add_argument('--base-label', required=True,
-                          help='Base label.',
-                          choices=classes)
-    arg_pars.add_argument('--model-path', required=True,
-                          help='Model path.')
-    arg_pars.add_argument('--batch-size', default=32, type=int,
-                          help='Size of batch of images.')
-    arg_pars.add_argument('--image-width', required=False,
-                          type=int,
-                          default=28)
-    arg_pars.add_argument('--latentdim', required=False,
-                          type=int,
-                          default=2)
-    return arg_pars.parse_args()
 
 
 def gen_batch(elem, batch_size):
@@ -60,11 +38,57 @@ def generate_image_by_character(models, label, batch_size, latent_parameters):
     )
 
 
-def generane_sentence_image(words_list, image_width, separator_width=5):
-    total_width = (image_width + separator_width) * \
-                  sum(map(lambda x: len(x), words_list)) - separator_width
+def generate_sentence_image(
+        words_list, models,
+        batch_size, latent_parameters, separator_width=5):
+    image_width = generate_image_by_character(
+        models, classes[0], batch_size, latent_parameters
+    ).shape[0]
+
+    total_width = image_width * sum(map(lambda x: len(x), words_list)) + \
+                  separator_width * (len(words_list) - 1)
 
     result_blank = np.zeros(shape=(image_width, total_width, 1), dtype=np.uint8)
+    result_blank += 255
+
+    character_lists = [
+        [
+            generate_image_by_character(
+                models, c, batch_size, latent_parameters
+            ) for c in word
+        ] for word in words_list
+    ]
+
+    shift_x = 0
+    for word_characters in character_lists:
+        for character in word_characters:
+            result_blank[:, shift_x:shift_x + character.shape[1]] = character
+            shift_x += character.shape[1]
+        shift_x += separator_width
+
+    return cv2.GaussianBlur(result_blank, (5, 5), 0)
+
+
+def argument_parser():
+    arg_pars = argparse.ArgumentParser()
+    arg_pars.add_argument('text', nargs='+',
+                          help='Text for translate.')
+    arg_pars.add_argument('--base-image', required=True,
+                          help='Base style image path.')
+    arg_pars.add_argument('--base-label', required=True,
+                          help='Base label.',
+                          choices=classes)
+    arg_pars.add_argument('--model-path', required=True,
+                          help='Model path.')
+    arg_pars.add_argument('--batch-size', default=32, type=int,
+                          help='Size of batch of images.')
+    arg_pars.add_argument('--image-width', required=False,
+                          type=int,
+                          default=28)
+    arg_pars.add_argument('--latentdim', required=False,
+                          type=int,
+                          default=2)
+    return arg_pars.parse_args()
 
 
 if __name__ == '__main__':
@@ -78,6 +102,8 @@ if __name__ == '__main__':
         dropout_rate=0.0
     )
 
+    print(models['cvae'].summary())
+
     models['cvae'].load_weights(args.model_path)
 
     latent_parameters = get_latent_parameters(
@@ -90,14 +116,19 @@ if __name__ == '__main__':
 
     print(latent_parameters)
 
-    gen_img = generate_image_by_character(
+    #gen_img = generate_image_by_character(
+    #    models,
+    #    'e',
+    #    args.batch_size,
+    #    latent_parameters
+    #)
+
+    gen_img = generate_sentence_image(
+        args.text,
         models,
-        'e',
         args.batch_size,
         latent_parameters
     )
-
-    print(models['cvae'].summary())
 
     window_name = 'Generated character'
     cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
